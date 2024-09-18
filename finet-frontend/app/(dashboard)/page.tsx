6fa724c0d6ebf745c1abd8aec034d2030787132d
Matthew Byrne <mattsbyrne90@gmail.com>
2024-09-18 13:38:11 -0700

    Implement base Query page

--- finet-frontend/app/(dashboard)/page.tsx
+++ finet-frontend/app/(dashboard)/page.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Link from 'next/link';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+import TextField from '@mui/material/TextField';
+import BasicLineChart from './Chart';
+import AutoInput from './AutoInput';
 
 export default function HomePage() {
   
 
   return (    
-      <Typography>
-        Welcome to <Link href="https://mui.com/toolpad/core/introduction">Toolpad Core!</Link>
-      </Typography>
+    <Paper>
+        <Grid container spacing={2}>
+            <Grid size={12} sx={{padding: 1}}>
+                <TextField fullWidth id="fullWidth" label="Enter stock" variant="outlined" />
+            </Grid>
+            <Grid size={12}>
+              <BasicLineChart />
+            </Grid>
+            <Grid size={12} sx={{padding: 1}}>
+              <TextField fullWidth id="fullWidth" label="Enter tags" variant="outlined" />
+            </Grid>
+        </Grid>
+    </Paper>
   );
 }
